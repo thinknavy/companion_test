db5a21ea9b6d8c9208a8e137a7162e0403c62be0
thinknavy <thinknavy@hotmail.com>
2019-05-28 15:47:11 +0200

    g

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -30,8 +30,8 @@
 
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
+        <div class="sidebar-brand-icon">
+          <i class="fas fa-building"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Component</div>
       </a>
